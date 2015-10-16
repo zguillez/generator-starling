7e4dc00d6d73e4aca91011cb70ae142d089ee49a
Guillermo de la Iglesia <zguillez@gmail.com>
2015-10-16 18:50:45 +0200

    fix bower reference

--- test/test-app.js
+++ test/test-app.js
@@ -1,24 +1,17 @@
 'use strict';
-
 var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
-
-describe('starling:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
-      .on('end', done);
+describe('starling:app', function() {
+  before(function(done) {
+    helpers.run(path.join(__dirname, '../generators/app')).withOptions({
+      skipInstall: true
+    }).withPrompts({
+      someOption: true
+    }).on('end', done);
   });
-
-  it('creates files', function () {
-    assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
+  it('creates files', function() {
+    assert.file(['package.json', '.editorconfig', '.jshintrc']);
   });
 });
